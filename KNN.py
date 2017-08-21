25be69a5852aeb59b3d573d094c5f3c6aa0d5dcb
Enzhao.Chen <tuh24535@temple.edu>
2017-08-21 16:17:53 -0400

    KNN models

--- KNN.py
+++ KNN.py
@@ -22,3 +22,17 @@ def classify0(inX, dataSet, labels, k):
         sortedClassCount = sorted(classCount.items(),
                                   key=operator.itemgetter(1), reverse=True)
     return sortedClassCount[0][0]
+
+def file2matrix(filename):
+    fr = open(filename)
+    numberOfLines = len(fr.readlines())
+    returnMat = zeros ((numberOfLines,3))
+    classLabelVector = []
+    fr  = open(filename)
+    index = 0
+    for line in fr.readlines():
+        listFromLine = line.split('\t')
+        returnMat[index,:] = listFromLine[0:3]
+        classLabelVector.append(int(listFromLine[-1]))
+        index += 1
+    return returnMat,classLabelVector
